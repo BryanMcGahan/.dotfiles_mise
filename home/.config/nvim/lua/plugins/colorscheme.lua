83cf50bbfa6fd7abecb3a2df06267f73b612db88
mise <mise@localhost>
2026-09-15 18:21:12 -0500

    edited mise/config.toml; added nvim/.gitignore, nvim/.neoconf.json, nvim/init.lua, nvim/lazy-lock.json, nvim/lazyvim.json +7 more
    Mise-History: {"trigger":"edit","description_source":"computed","task":null,"labels":[],"omitted":["config/config.local.toml"],"incomplete":[],"operation":null}

---
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  -- add gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      require("gruvbox").setup({ contrast = "hard" })
+    end,
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
---
+++ home/.config/nvim/lua/plugins/modified.lua
@@ -0,0 +1,49 @@
+return {
+
+  -- change trouble config
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
+
+  -- override nvim-cmp and add cmp-emoji
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
+  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
+  -- would overwrite `ensure_installed` with the new value.
+  -- If you'd rather extend the default config, use the code below instead:
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "tsx",
+        "typescript",
+      })
+    end,
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            auto_close = true,
+            layout = {
+              layout = {
+                position = "right",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
